0cf90abfcefbc0f7fc8feaead6750c506dc2ca13
Elsa Brown <elsambrown@gmail.com>
2021-11-05 16:51:17 -0400

    adds files

---
+++ index.js
@@ -0,0 +1,37 @@
+const paths = document.querySelectorAll('path');
+let idxA = -1;
+let idxB = paths.length + 1;
+let count = 0;
+ 
+const fillPath = () => {
+   count++;
+   idxB--;
+   const currPathB = paths[idxB];
+ 
+   if (currPathB) {
+       if (idxB % 2) {
+       currPathB.setAttribute('style', `fill:#fff;`);
+       } else {
+       currPathB.setAttribute('style', `fill:#000`);
+       }
+   }
+ 
+   if (count >= 2000) {
+       idxA++;
+       const currPathA = paths[idxA];
+      
+       if (currPathA) {
+       if (idxA % 2) {
+           currPathA.setAttribute('style', `fill:#fff`);
+       } else {
+           currPathA.setAttribute('style', `fill:#000`);
+       }
+   }
+};
+ 
+   window.requestAnimationFrame(fillPath);
+ 
+};
+ 
+window.requestAnimationFrame(fillPath);
+
