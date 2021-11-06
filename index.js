3bf762e93d3e0cab677d08d2061c331c3bae4d33
Elsa Brown <elsambrown@gmail.com>
2021-11-06 09:31:30 -0400

    mod outline opacity, idx fix, mini refactor

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const paths = document.querySelectorAll('path');
 const svg = document.querySelector('svg');
 const pathLength = 14072;
 
-let idxA = 0;
-let idxB = paths.length + 1;
+let idxA = -1;
+let idxB = paths.length + 5;
 let count = 0;
 let offset = 0;
 let filterBlur = 0;
@@ -15,48 +15,52 @@ const fillPath = () => {
   idxB--;
   const currPathB = paths[idxB];
   if (count >= 150) {
-    if (currPathB) {
+    if (currPathB && !currPathB.classList.contains('done')) {
       if (idxB % 2) {
         currPathB.setAttribute('style', `fill:#fff;`);
+        currPathB.classList.add('done');
       } else {
         currPathB.setAttribute('style', `fill:#000`);
+        currPathB.classList.add('done');
       }
     }
   }
 
   idxA++;
   const currPathA = paths[idxA];
-  if (currPathA) {
+  if (currPathA && !currPathA.classList.contains('done')) {
     if (idxA % 2) {
       currPathA.setAttribute('style', `fill:#fff`);
+      currPathA.classList.add('done');
     } else {
       currPathA.setAttribute('style', `fill:#000`);
+      currPathA.classList.add('done');
     }
+  }
 
-    if (offset > -520) {
-      if (count > 1760 && count < 1820) {
-        offset += 0.25;
-        svg.setAttribute('style', `outline-offset: ${offset}px`);
-      }
-      if (count > 1900 && count < 1940 ||
-        count > 4000 && count <= 4080 ||  
-        count > 5600 && count < 5650 ||
-        count > 6000) {
-        offset--;
-        svg.setAttribute('style', `outline-offset: ${offset}px`);
-      }
-    } else if (filterBlur < 14 && !reverseBlur) {
-      filterBlur += 0.10;
-      svg.setAttribute('style', `filter: blur(${filterBlur}px); outline-offset: ${offset}px`);
-    } else if (filterBlur >= 14 && !reverseBlur) {
-      reverseBlur = true;
-    } else if (filterBlur > 0 && reverseBlur) {
-      filterBlur -= 0.10;
-      svg.setAttribute('style', `filter: blur(${filterBlur}px); outline-offset: ${offset}px`);
+  if (offset > -520) {
+    if (count > 1760 && count < 1820) {
+      offset += 0.25;
+      svg.setAttribute('style', `outline-offset: ${offset}px`);
     }
+    if (count > 1900 && count < 1940 ||
+      count > 4000 && count <= 4080 ||  
+      count > 5600 && count < 5650 ||
+      count > 6000) {
+      offset--;
+      svg.setAttribute('style', `outline-offset: ${offset}px`);
+    }
+  } else if (filterBlur < 14 && !reverseBlur) {
+    filterBlur += 0.10;
+    svg.setAttribute('style', `filter: blur(${filterBlur}px); outline-offset: ${offset}px`);
+  } else if (filterBlur >= 14 && !reverseBlur) {
+    reverseBlur = true;
+  } else if (filterBlur > 0 && reverseBlur) {
+    filterBlur -= 0.10;
+    svg.setAttribute('style', `filter: blur(${filterBlur}px); outline-offset: ${offset}px`);
   }
 
   window.requestAnimationFrame(fillPath);
 };
  
-window.requestAnimationFrame(fillPath);
+  window.requestAnimationFrame(fillPath);
